2d84544f736702e0489917bb0fa026d1057fff40
lesternoronha <lester_noronha@yahoo.com>
2017-11-12 21:18:11 +1100

    RESTServiceClient.java
    RESTServiceClient.java

--- src/main/java/com/weatherapp/client/RESTServiceClient.java
+++ src/main/java/com/weatherapp/client/RESTServiceClient.java
@@ -1,1 +1,34 @@
+/**
+ * RESTServiceClient.java
+ *
+ * Created by Di007Va
+ * Created on Jun 11, 2013
+ *
+ *
+ * Copyright (c) 2013 Symbiont Softwares
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Symbiont Softwares.  You shall use it only in accordance with the terms
+ * of the license agreement you entered into with Symbiont Softwares.
+ *
+ */
+package main.java.com.weatherapp.client;
 
+public interface RESTServiceClient {
+
+	/**
+	 *
+	 * @param request
+	 * @return
+	 */
+	public Object getXMLResponse(Object request);
+
+	/**
+	 *
+	 * @param request
+	 * @return
+	 */
+	public Object getJSONResponse(Object request);
+
+}
